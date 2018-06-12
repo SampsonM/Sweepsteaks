d0e6bb56a36dfbb165bbba6ad0aa1a199803bd85
SampsonM <SampsonM@github.com>
2018-06-12 22:00:55 +0100

    set up registeration page

---
+++ FE/sweepstakesvue/src/components/Register.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Register Page</h1>
+
+    <input 
+      type="email"
+      name="email"
+      v-model="email"
+
+      placeholder="email" />
+      <br>
+    <input 
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password" />
+    <br>
+    <button
+      @click="register">
+      register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  },
+  mounted () {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
