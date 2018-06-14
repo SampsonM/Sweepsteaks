41f602f69f736aa0aef2c152177af2b13a5b07de
SampsonM <SampsonM@github.com>
2018-06-14 11:18:22 +0100

    created fake data

---
+++ BE/db/data/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  competitionData: require('./Competition.json'),
+  groupData: require('./Group.json'),
+  sportData: require('./Sport.json'),
+  teamData: require('./Team.json'),
+  userData: require('./User.json')
+}
