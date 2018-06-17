194bc6100c4f7eab416a8307a6e7e8d1188e8690
SampsonM <SampsonM@github.com>
2018-06-17 21:17:47 +0100

    Started creating login and create account forms: FE

--- FE/sweepstakes/src/components/welcome.js
+++ FE/sweepstakes/src/components/welcome.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import '../styles/welcome.css';
 
 class Welcome extends Component {
   render () {
     return (
-      <div>
-        <p>Welcome to Sweepstakes!!</p>
+      <div className="background">
+        <main>
+          <img src="https://ecommerceinsiders.com/wp-content/uploads/2015/06/sweepstakes.jpg" 
+            alt="Sweepstakes Logo" className="logo"/>
+          
+          <Form props={this.state} />
+        </main>
       </div>
     )
   }
+}
+
+const Form = (props) => {
+  this.setState = props;
+
+  return (
+    <form>
+      <button>Login</button>
+      <button>Create Account</button>
+      {this.state.login && <LoginForm />}
+      {this.state.createAccount && <CreateForm />}
+    </form>
+  )
+}
+
+const LoginForm = () => {
+  return (
+    <div>Login form</div>
+  )
+}
+
+const CreateForm = () => {
+  return (
+    <div>create account form</div>
+  )
 }
 
 export default Welcome;
