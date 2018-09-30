c1654a0dbd843d366b7d56d834ab3c909691d262
SampsonM <SampsonM@github.com>
2018-09-30 17:26:51 +0100

    Added fonts and styling, finished final animation for cross device compatibilty: FE

--- FE/vue-sweepstakes/src/App.vue
+++ FE/vue-sweepstakes/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>&nbsp;|&nbsp;
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -10,13 +10,14 @@
 
 <style lang="scss">
 @import "./styles/_vars.scss";
+@import "./styles/_mixins.scss";
 
 * {
   margin: 0;
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: $font, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -24,15 +25,25 @@
   background-size: cover;
   min-height: 100vh;
   min-width: 100vw;
+  overflow: hidden;
 }
+
 #nav {
   padding: 30px;
+  display: flex;
+  justify-content: center;
   a {
     font-weight: bold;
     color: $dark-blue;
+    text-decoration: none;
     &.router-link-exact-active {
       color: $light-blue;
     }
+  }
+
+  @include breakpoint(tablet) {
+    justify-content: flex-end;
+    font-size: 1.5em;
   }
 }
 </style>
--- FE/vue-sweepstakes/src/views/Home.vue
+++ FE/vue-sweepstakes/src/views/Home.vue
@@ -5,28 +5,27 @@
       <img class="steak" src="../assets/meat.svg" />
       <img class="steak" src="../assets/meat.svg" />
     </div>
-    <HelloWorld msg="Welcome to Sweep-steaks"/>
+    <h1 class="title">- welcome to -<br/>SWEEP-STEAKS</h1>
   </div>
 </template>
 
 <script>
 
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 <style lang="scss" scoped>
+@import "../styles/_mixins.scss";
+@import "../styles/_vars.scss";
+
 @-webkit-keyframes animateCloud {
     0% {
         margin-left: -1100px;
     }
     100% {
-        margin-left: 18%;
+        margin-left: 30%;
     }
 }
 
@@ -35,7 +34,7 @@ export default {
         margin-left: -1100px;
     }
     100% {
-        margin-left: 18%;
+        margin-left: 30%;
     }
 }
 
@@ -44,86 +43,119 @@ export default {
         margin-left: -1100px;
     }
     100% {
-        margin-left: 18%;
+        margin-left: 30%;
     }
 }
 
 @-webkit-keyframes fade {
     0% {
       opacity: 1;
-      left: -6%;
+      left: -8%;
     }
-    75% {
+    90% {
       opacity: 0.6;
-      left: -6%;
+      left: -8%;
 
     }
     100% {
       opacity: 0;
-      left: 70px;
+      left: 8px;
     }
 }
 
 @-moz-keyframes fade {
     0% {
       opacity: 1;
-      left: -6%;
+      left: -8%;
     }
-    75% {
+    90% {
       opacity: 0.6;
-      left: -6%;
+      left: -8%;
     }
     100% {
       opacity: 0;
-      left: 70px;
+      left: 8px;
     }
 }
 
 @keyframes fade {
     0% {
       opacity: 1;
-      left: -6%;
+      left: -8%;
     }
-    75% {
+    90% {
       opacity: 0.6;
-      left: -6%;
+      left: -8%;
     }
     100% {
       opacity: 0;
-      left: 70px;
+      left: 8px;
     }
 }
 
 .animated-image {
-  margin-left: 18%;
   display: flex;
-	-webkit-animation: animateCloud 2s ease-in-out 1;
-	-moz-animation: animateCloud 2s ease-in-out 1;
-	animation: animateCloud 2s ease-in-out 1;
-	
-	-webkit-transform: scale(0.25);
-	-moz-transform: scale(0.25);
-	transform: scale(0.25);
+  margin: 80px 0 0 30%;
+
+	-webkit-animation: animateCloud 1.4s ease-in-out 1;
+	-moz-animation: animateCloud 1.4s ease-in-out 1;
+  animation: animateCloud 1.4s ease-in-out 1;
+
+  @include breakpoint(tablet) {
+    margin-top: 23%;
+  }
+
+  @include breakpoint(desktop) {
+    margin-top: 18%;
+  }
+
 }
 
 .brush {
-  -webkit-transform: scale(4.3);
-	-moz-transform: scale(4.3);
-	transform: scale(4.3);
+  -webkit-transform: scale(2.2);
+	-moz-transform: scale(2.2);
+  transform: scale(2.2);
+  
+  @include breakpoint(tablet) {
+    -webkit-transform: scale(4.3);
+    -moz-transform: scale(4.3);
+    transform: scale(4.3);
+  }
 }
 
 .steak {
-  -webkit-transform: scale(1.8);
-	-moz-transform: scale(1.8);
-  transform: scale(1.8);
-  
   position: relative;
-  top: 260px;
-  -webkit-animation: fade 2s ease-in-out 1;
-	-moz-animation: fade 2s ease-in-out 1;
-  animation: fade 2s ease-in-out 1;
+  top: 110px;
   opacity: 0;
-} 
+
+  -webkit-animation: fade 1.4s ease-in-out 1;
+	-moz-animation: fade 1.4s ease-in-out 1;
+  animation: fade 1.4s ease-in-out 1;
+
+  @include breakpoint(tablet) {
+    top: 270px;
+  };
+}
+
+.title {
+  margin-top: 35%;
+  left:0;
+  right:0;
+  color: $red;
+
+  @include breakpoint(mobileL) {
+    position: absolute;
+    bottom: 0;
+    margin-left: 60%;
+  }
+
+  @include breakpoint(tablet) {
+    position: absolute;
+    bottom: 20%;
+    margin-left: 30%;
+    font-size: 5em;
+  }
+}
 
 </style>
 
