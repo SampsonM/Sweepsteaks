2d2eb92ecf41bc4a146d836bb25ff3c33802ecd7
SampsonM <SampsonM@github.com>
2019-04-24 10:56:56 +0100

    Reformatted listen js to run different servers dependent on environment test/dev/prod

--- BE/src/listen.js
+++ BE/src/listen.js
@@ -1,7 +1,25 @@
 import app from './app.js';
+import https from 'https';
 import http from 'http';
-const { PORT } = process.env || require('../config');
+import fs from 'fs';
+import { PORT } from '../config';
+import path from 'path';
 
-http.createServer(app).listen(PORT, () => {
-  console.log(`listening on PORT: ${PORT} https://localhost:${PORT}/api/`)
-});
+if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production") {
+  const options = {
+    key: fs.readFileSync(path.resolve(__dirname, '../config/certs/rootCA.key'), 'utf8'),
+    cert: fs.readFileSync(path.resolve(__dirname, '../config/certs/rootCA.pem'), 'utf8'),
+    passphrase: 'B@gbrush1'
+  };
+  
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`HTTPS listening on PORT: ${PORT} https://localhost:${PORT}/api`)
+  });
+}
+
+// Runs on test to avoid HTTPS config using supertest
+if (process.env.NODE_ENV === "test") {
+  http.createServer(app).listen(8081, () => {
+    console.log(`HTTP listening on PORT: 8081 http://localhost:8081/api`)
+  });
+}
