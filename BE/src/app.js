0d984156ed6f5e504762b0a882d3d23580fd121c
SampsonM <SampsonM@github.com>
2019-04-24 15:12:17 +0100

    Updated listen to use https on dev server

--- BE/src/app.js
+++ BE/src/app.js
@@ -63,4 +63,4 @@ app.use((err, req, res, next) => {
   return res.status(500).send(err);
 });
 
-module.exports = app;
+export default app;
--- BE/src/listen.js
+++ BE/src/listen.js
@@ -1,8 +1,28 @@
 import app from './app.js';
 import http from 'http';
+import https from 'https';
+import path from 'path';
+import fs from 'fs';
 
 const PORT = process.env.PORT || require('../config').PORT;
 
-http.createServer(app).listen(PORT, () => {
-  console.log(`HTTP listening on PORT: ${PORT} http://localhost:${PORT}/api`)
-});
+if (process.env.NODE_ENV === 'production') {
+  http.createServer(app).listen(PORT, () => {
+    console.log(`HTTP listening on PORT: ${PORT} http://localhost:${PORT}/api`)
+  });
+}
+
+// Keep HTTPS to run localhost on chrome
+// due to chrome updating the scheme from HTTP to HTTPS
+// Heroku does not reauire this however so we keep HTTP
+if (process.env.NODE_ENV === 'development') {
+  const options = {
+    key: fs.readFileSync(path.resolve(__dirname, '../config/certs/rootCA.key'), 'utf8'),
+    cert: fs.readFileSync(path.resolve(__dirname, '../config/certs/rootCA.pem'), 'utf8'),
+    passphrase: 'B@gbrush1'
+  };
+
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`HTTPS listening on PORT: ${PORT} https://localhost:${PORT}/api`)
+  })
+}
