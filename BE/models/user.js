2237f22ffd93751d0bf99860ae9563110f00484d
SampsonM <SampsonM@github.com>
2019-04-25 22:07:53 +0100

    Added jwt functions to user schema for authentication

--- BE/models/user.js
+++ BE/models/user.js
@@ -1,5 +1,7 @@
 'use strict';
 import mongoose from 'mongoose';
+import { createHashSalt } from '../utils';
+import jwt from 'jsonwebtoken';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -43,5 +45,37 @@ const UserSchema = new Schema({
     }
   }]
 });
+
+
+UserSchema.methods.setHash = function(password) {
+  const { salt, hash } = createHashSalt(password)
+
+  this.salt = salt;
+  this.hash = hash
+};
+
+UserSchema.methods.validatePassword = function(password) {
+  return this.hash === hash;
+};
+
+UsersSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    email: this.email,
+    id: this._id,
+    exp: parseInt(expirationDate.getTime() / 1000, 10),
+  }, 'secret');
+}
+
+UsersSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id,
+    email: this.email,
+    token: this.generateJWT()
+  };
+};
 
 module.exports = mongoose.model('users', UserSchema);
