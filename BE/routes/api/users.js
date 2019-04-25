12de20f0c8dde44132e103cb62081843f092e1da
SampsonM <SampsonM@github.com>
2019-04-25 22:08:23 +0100

    Added auth function to user log in and creation routes

--- BE/routes/api/users.js
+++ BE/routes/api/users.js
@@ -1,23 +1,24 @@
 'use strict'
-const usersRouter = require('express').Router()
-import * as usersCtrl from '../../controllers/users'
+const usersRouter = require('express').Router();
+import auth from '../auth';
+import * as usersCtrl from '../../controllers/users';
 
 //GET User by name
-usersRouter.get('/:user_name', usersCtrl.getUserByName)
+usersRouter.get('/:user_name', usersCtrl.getUserByName);
 
 //GET current route (required, only authenticated users have access)
-usersRouter.get('/current', usersCtrl.userLoggedIn)
+usersRouter.get('/current', auth.required, usersCtrl.userLoggedIn);
 
 //POST new user route (optional, everyone has access)
-usersRouter.post('/', usersCtrl.createUser)
+usersRouter.post('/', auth.optional, usersCtrl.createUser);
 
 //POST login route (optional, everyone has access)
-usersRouter.post('/login', usersCtrl.logUserIn)
+usersRouter.post('/login', auth.optional, usersCtrl.logUserIn);
 
 //POST to update user
-usersRouter.post('/:user_id', usersCtrl.updateUser)
+usersRouter.post('/:user_id', usersCtrl.updateUser);
 
 //DELETE USER
-usersRouter.delete('/:user_id', usersCtrl.deleteUser)
+usersRouter.delete('/:user_id', usersCtrl.deleteUser);
 
 module.exports = usersRouter;
