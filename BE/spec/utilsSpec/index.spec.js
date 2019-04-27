35883f27192b95191c78ec76858842b82325798c
SampsonM <SampsonM@github.com>
2019-04-27 12:32:05 +0100

    Removed test for timestamp check: BE

--- BE/spec/utilsSpec/index.spec.js
+++ BE/spec/utilsSpec/index.spec.js
@@ -1,22 +1,8 @@
 'use strict'
 import { expect } from 'chai'
-import utils from '../../utils'
+import * as utils from '../../utils'
 
 describe('utils file functions test', () => {
-  it('validates time stamp is current', () => {
-
-    const date = new Date().toISOString()
-    const request = {
-      body: {
-        sync: date
-      }
-    }
-
-    const result = utils.timestampValid(request)
-    
-    expect(result).to.equal(true)
-  })
-
   it('validates if request is NOT GET', () => {
 
     const request = {
