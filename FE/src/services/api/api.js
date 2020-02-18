8802376e6232890c114efb4fb4f91f5f6f1dae62
SampsonM <SampsonM@github.com>
2020-02-18 20:23:42 +0000

    FE: Changed base url to match CF

--- FE/src/services/api/api.js
+++ FE/src/services/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Vue from 'vue'
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'https://localhost:3000/api',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://api.sweepsteaks.co.uk/api' : 'https://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json'
   },
