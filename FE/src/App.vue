5db5f44f25482fda704edc8ef18789e0cec34d5e
SampsonM <SampsonM@github.com>
2020-03-26 21:47:15 +0000

    FE: Fixed scaling bg image

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -12,11 +12,16 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background: url('./assets/img/wood.svg') no-repeat fixed center;
-  background-size: cover;
-  height: 100%;
+  background: url('./assets/img/wood.svg') no-repeat center top fixed;
+  background-size: 310% auto;
+  height: 100vh;
   min-width: 100vw;
   overflow-x: hidden;
   overflow-y: scroll;
+
+  @include breakpoint(tablet) {
+    height: 100%;
+    background-size: cover;
+  }
 }
 </style>
