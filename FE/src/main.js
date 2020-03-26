9876f1be5b2b31d568099d39360782fb62323a97
SampsonM <SampsonM@github.com>
2020-03-26 23:17:33 +0000

    FE: Changed cookie session length

--- FE/src/main.js
+++ FE/src/main.js
@@ -13,7 +13,7 @@ Vue.use(Vuelidate)
 
 const url = process.env.NODE_ENV === 'development' ? 'localhost' : 'sweepsteaks.co.uk'
 
-Vue.$cookies.config(null, null, url, true)
+Vue.$cookies.config('1d', null, url, true)
 
 featureFlagService.enableFeatureFlags()
 Vue.config.productionTip = false
